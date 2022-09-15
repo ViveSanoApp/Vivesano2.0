3a8754416fa277f563e822fa5e2911f0dda6896a
Andres Developer <96069440+ViveSanoApp@users.noreply.github.com>
2022-09-15 13:35:20 -0500

    Add files via upload

---
+++ btn.js
@@ -0,0 +1,14 @@
+document.querySelector('.nav__links').addEventListener('click', () =>{
+    document.querySelector('.container__all').classList.toggle("show")
+  });
+
+  document.getElementById('.menu__btn').addEventListener('click', () =>{
+    document.querySelector('.modal-container').classList.toggle("up")
+  });
+
+
+
+
+
+
+
